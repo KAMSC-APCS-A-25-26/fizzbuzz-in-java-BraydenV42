ea8c0a9d6f4350e1253f9c65ad457672edfd6ef8
BraydenV42 <brayriley44@gmail.com>
2025-09-18 22:24:36 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,14 +1,17 @@
 public class FizzBuzz {
-    /**
-     * Print the FizzBuzz sequence for 1..100 to standard output.
-     * Rules:
-     * - Multiples of 3 -> "Fizz"
-     * - Multiples of 5 -> "Buzz"
-     * - Multiples of both 3 and 5 -> "FizzBuzz"
-     * - Otherwise -> the number itself
-     * Output should be 100 whitespace-separated tokens (spaces or newlines are fine).
-     */
     public static void main(String[] args) {
-        // TODO
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.print("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz");
+            } else {
+                System.out.print(i);
+            }
+            // Add a space after each token
+            System.out.print(" ");
+        }
     }
 }
