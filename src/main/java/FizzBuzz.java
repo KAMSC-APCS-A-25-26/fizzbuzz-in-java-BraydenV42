637d3cd4180ba08f40c8ef87434a0648e44621cc
BraydenV42 <brayriley44@gmail.com>
2025-09-18 22:49:35 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,13 +2,13 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0) {
-                System.out.print("FizzBuzz ");
+                System.out.println("FizzBuzz ");
             } else if (i % 3 == 0) {
-                System.out.print("Fizz ");
+                System.out.println("Fizz ");
             } else if (i % 5 == 0) {
-                System.out.print("Buzz ");
+                System.out.println("Buzz ");
             } else {
-                System.out.print(i + " ");
+                System.out.println(i + " ");
             }
         }
     }
